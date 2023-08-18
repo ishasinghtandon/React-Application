2ddb683a3f7b45c9e16f8120d69b0cf70f40ec05
Shaina <85868072+ishasinghtandon@users.noreply.github.com>
2023-08-18 21:33:37 +0530

    Create secondpage.tsx

---
+++ secondpage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataGridComponent from '../components/DataGridComponent';
+
+const SecondPage: React.FC = () => {
+  return (
+    <div>
+      <h2>Second Page</h2>
+      <DataGridComponent />
+    </div>
+  );
+};
+
+export default SecondPage;
